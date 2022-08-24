5c969b186afad2fb972d1df888f4e727fe4561ad
Bledi Feshti <bledi.feshti@gmail.com>
2022-08-24 09:11:47 +0200

    Release Neo4j 4.4.10

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -7,7 +7,7 @@ CLUSTER=lab
 ZONE=us-east1-b
 NODES=4
 API=beta
-NEO4J_VERSION=4.4.8-enterprise
+NEO4J_VERSION=4.4.10-enterprise
 
 gcloud beta container clusters create $CLUSTER \
     --zone "$ZONE" \
