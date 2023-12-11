94e19e87ed559978dab479465a43a5de469c05e1
bfeshti <bledi.feshti@neo4j.com>
2023-12-11 23:52:23 +0100

    Added timestamp to k8s cluster name

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -3,11 +3,13 @@
 # This script is intended to be used for internal testing only, to create the artifacts necessary for 
 # testing and deploying this code in a sample GKE cluster.
 PROJECT=neo4j-k8s-marketplace-public
-CLUSTER=lab
+CLUSTER_PREFIX=lab
 ZONE=us-east1-b
 NODES=4
 API=beta
 NEO4J_VERSION=4.4.26-enterprise
+
+CLUSTER=${CLUSTER_PREFIX}-$(date +"%Y%m%d%H%M%S")
 
 gcloud beta container clusters create $CLUSTER \
     --zone "$ZONE" \
